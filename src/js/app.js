127ddcf9af8a2fdb813b7e0f0555faab82dbc8d7
Nikita Peplov <npeplov@yandex.ru>
2021-03-11 10:32:22 +0300

    fixed genPosition max value, removed firstIndex

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,26 @@
 function genPosition() {
-  return Math.floor(Math.random() * (16 - 0)) + 0;
+  return Math.floor(Math.random() * (15 - 0 + 1)) + 0;
 }
 
 const parentDiv = document.getElementsByClassName('wrapper')[0];
-const firstIndex = genPosition();
 const img = document.images[0];
 img.parentElement.removeChild(img);
 
 for (let i = 0; i < 4 * 4; i += 1) {
   const div = document.createElement('div');
   parentDiv.appendChild(div);
-  if (firstIndex === i) div.appendChild(img);
 }
 
 function setActive() {
-  const activeDiv = document.images[0].parentElement;
-  activeDiv.removeChild(img);
+  if (document.images[0]) {
+    const activeDiv = document.images[0].parentElement;
+    activeDiv.removeChild(img);
+  }
 
   const childDivs = parentDiv.children;
   const index = genPosition();
   childDivs[index].appendChild(img);
 }
 
-const timerId = setInterval(setActive, 1500);
-setTimeout(() => { clearInterval(timerId); }, 5000);
+const timerId = setInterval(setActive, 500);
+setTimeout(() => { clearInterval(timerId); }, 10000);
